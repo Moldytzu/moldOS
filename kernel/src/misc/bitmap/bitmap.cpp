694f299fba782cae158cff56376ca44e6180577a
Moldytzu <xmalecsxxrovertebral@yahoo.com>
2021-08-22 11:53:48 +0300

    Optimized bitmap

--- kernel/src/misc/bitmap/bitmap.cpp
+++ kernel/src/misc/bitmap/bitmap.cpp
@@ -6,23 +6,12 @@ bool Bitmap::operator[](uint64_t index){
 
 bool Bitmap::Get(uint64_t index){
     if(index >Size * 8) return false;
-    uint64_t byteIndex = index/8;
-    uint8_t bitIndex = index%8;
-    uint8_t bitIndexer = 0b10000000 >> bitIndex;
-    if((Buffer[byteIndex] & bitIndexer) > 0) {
-        return true;
-    }
-    return false;
+    return (Buffer[index/8] & (0b10000000 >> index%8)) > 0;
 }
 
 bool Bitmap::Set(uint64_t index, bool value) {
     if(index >Size * 8) return false;
-    uint64_t byteIndex = index/8;
-    uint8_t bitIndex = index%8;
-    uint8_t bitIndexer = 0b10000000 >> bitIndex;
-    Buffer[byteIndex] &= ~bitIndexer;
-    if(value) {
-        Buffer[byteIndex] |= bitIndexer;
-    }
+    Buffer[index/8] &= ~0b10000000 >> index%8;
+    if(value) Buffer[index/8] |= 0b10000000 >> index%8;
     return true;
 }
