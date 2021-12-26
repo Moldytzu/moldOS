f1b597b75f101ff8f120dca0441e2cb8004e54bc
Moldytzu <xmalecsxxrovertebral@yahoo.com>
2021-12-26 22:28:08 +0200

    separating parameters

--- kernel/src/terminal/ansi.cpp
+++ kernel/src/terminal/ansi.cpp
@@ -12,13 +12,44 @@ void ANSIPrint(const char* text)
             if(index+1 < strlen(text) && text[index+1] == '[')
             {
                 index+=2;
+
+                //buffers to store the parameters individualy
+                char param1[2];
+                char param2[2];
+
+                //clear them
+                memset(param1,0,2);
+                memset(param2,0,2);
+
+                bool switched = false;
+                int paramIndex = 0;
                 //loop thru until an 'm' appears
                 do
                 {
-                    GlobalDisplay->putc(text[index]);
+                    if(text[index] == ';')
+                    {
+                        paramIndex = 0;
+                        switched = true;
+                    }
+                    else
+                    {
+                        if(switched)
+                            param2[paramIndex] = text[index];
+                        else
+                            param1[paramIndex] = text[index];
+                        paramIndex++;
+                    }
                     index++;
                 }
                 while(text[index] != 'm');
+
+                GlobalDisplay->puts(" First param is ");
+                GlobalDisplay->putc(param1[0]);
+                GlobalDisplay->putc(param1[1]);
+                GlobalDisplay->puts(" Second param is ");
+                GlobalDisplay->putc(param2[0]);
+                GlobalDisplay->putc(param2[1]);
+                GlobalDisplay->putc(' ');
             }
             continue;
         }
