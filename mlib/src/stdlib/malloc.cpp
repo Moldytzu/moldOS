3d43fcdf6915cc7bfccc7b33f1a626171746ba9c
Moldytzu <xmalecsxxrovertebral@yahoo.com>
2021-12-27 20:01:34 +0200

    allocate more only if it's not alligned to 4k

--- mlib/src/stdlib/malloc.cpp
+++ mlib/src/stdlib/malloc.cpp
@@ -2,5 +2,7 @@
 
 void* malloc(size_t size)
 {
-    return (void*)sys_call(SYS_ALLOCATEMEMORY,size/4096+1,0,0,0);
+    uint64_t pages = size/4096;
+    if(size % 4096 != 0) pages++;
+    return (void*)sys_call(SYS_ALLOCATEMEMORY,pages,0,0,0);
 }
