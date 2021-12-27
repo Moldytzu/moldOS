06dcdc5045dbc875ceaed70575d47069b103311d
Moldytzu <xmalecsxxrovertebral@yahoo.com>
2021-12-27 22:05:38 +0200

    comment userspace.cpp

--- kernel/src/userspace/userspace.cpp
+++ kernel/src/userspace/userspace.cpp
@@ -24,67 +24,67 @@ uint64_t SyscallHandler(uint64_t syscall, uint64_t arg1, uint64_t arg2, uint64_t
     switch (syscall)
     {
     case SYSCALL_WRITE:
-        if(arg2 == SOUT) SysSerialWrite((char)arg1);
-        else SysConsoleWrite((char)arg1);
+        if(arg2 == SOUT) SysSerialWrite((char)arg1); //write on serial
+        else SysConsoleWrite((char)arg1); //write on tty
         break;
     case SYSCALL_EXIT:
-        Exit(arg1);
+        Exit(arg1); //exit
         break;
     case SYSCALL_OPEN:
     {
-        FileDescriptor* descriptor = (FileDescriptor*)GlobalAllocator.RequestPage();
-        memset((void*)descriptor,0,4096);
-        GlobalTableManager.MapUserspaceMemory((void*)descriptor);
-        memcpy(descriptor,VFSOpenFile((const char*)(void*)(uint64_t)arg1),sizeof(FileDescriptor));
-        return (uint64_t)(void*)descriptor;
+        FileDescriptor* descriptor = (FileDescriptor*)GlobalAllocator.RequestPage(); //allocate a descriptor
+        memset((void*)descriptor,0,4096); //clear it
+        GlobalTableManager.MapUserspaceMemory((void*)descriptor); //map it
+        memcpy(descriptor,VFSOpenFile((const char*)(void*)(uint64_t)arg1),sizeof(FileDescriptor)); //copy data into it
+        return (uint64_t)(void*)descriptor; //return it
         break;
     }
     case SYSCALL_ALLOCATEMEMORY:
     {
         if(!arg1) return 0;
         CurrentTask.memoryUse += arg1 * 4096; //it allocates pages, each being 4 kilobytes
-        void* mem = GlobalAllocator.RequestPages(arg1);
+        void* mem = GlobalAllocator.RequestPages(arg1); //request pages
         for(int i = 0; i<=4096*arg1; i++)
-            GlobalTableManager.MapUserspaceMemory((void*)((uint64_t)mem+i));
+            GlobalTableManager.MapUserspaceMemory((void*)((uint64_t)mem+i)); //map them
 #ifdef Debugging_Scheduler
         SerialWrite(CurrentTask.name," is now using in total ", inttostr(CurrentTask.memoryUse/1024), " KB, while the whole operating system using only ");
         SerialWrite(inttostr(GlobalAllocator.GetUsedRAM()/1024/1024), " MB\n");
 #endif
-        return (uint64_t)mem;
+        return (uint64_t)mem; //return pointer
         break;
     }
     case SYSCALL_READ:
     {
-        memcpy((void*)arg1,VFSReadFile((FileDescriptor*)(void*)arg2),arg3);
+        memcpy((void*)arg1,VFSReadFile((FileDescriptor*)(void*)arg2),arg3); //copy buffer
         break;
     }
     case SYSCALL_GETTERMINALOUTPUT:
-        return (uint64_t)(uint64_t*)VirtualTerminals[arg1].buffer;
+        return (uint64_t)(uint64_t*)VirtualTerminals[arg1].buffer; //get buffer
         break;
     case SYSCALL_GETPROCESSTERMINAL:
-        return CurrentTask.terminal;
+        return CurrentTask.terminal; //get terminal
         break;
     case SYSCALL_GETPIDBYNAME:
     {
         for(int i = 0; i<1024; i++)
         {
-            if(memcmp((const void*)arg1,GlobalTaskManager->tasks[i].name,strlen(GlobalTaskManager->tasks[i].name))==0)
+            if(memcmp((const void*)arg1,GlobalTaskManager->tasks[i].name,strlen(GlobalTaskManager->tasks[i].name))==0) //check every task's name
                 return i;
         }
         return 0xFFFFFFFFFFFFFFFF;
         break;
     }
     case SYSCALL_GETPID:
-        return GlobalTaskManager->currentTask;
+        return GlobalTaskManager->currentTask; //get pid
         break;
     case SYSCALL_GETTERMINALBYPID:
-        return GlobalTaskManager->tasks[arg1].terminal;
+        return GlobalTaskManager->tasks[arg1].terminal; //get terminal of a pid
         break;
     default:
-        LogWarn(inttohstr((uint64_t)syscall));
+        LogWarn(inttohstr((uint64_t)syscall)); //warn
         LogWarn("Unknown syscall!");
         return ERROR_UNKNOWN_SYSCALL;
         break;
     }
-    return 512;
+   return 512;
 }
